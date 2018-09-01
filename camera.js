105d708dc86c7c3618669b3ea38b4e6819f87232
J-Sexton <josh.b.sexton@gmail.com>
2018-09-01 14:53:40 -0400

    Initial commit

---
+++ camera.js
@@ -0,0 +1,17 @@
+const camera = require('pi-camera');
+const myCamera = new camera({
+    mode: 'video',
+    output: `${__dirname}/video.h264`,
+    width : 960,
+    height: 540,
+    timeout: 5000,
+    preview: true,
+});
+
+myCamera.record()
+    .then((result) => {
+       console.log('captured video');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
